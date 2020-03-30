fb7a8e78db399783bb1da888fc46586c3a31ce84
Sunse7 <sunsetluna@gmail.com>
2020-03-30 19:08:15 +0200

    Created Movie Model and scaffolded

---
+++ WebbLab3/Data/WebbLab3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebbLab3.Models;
+
+namespace WebbLab3.Data
+{
+    public class WebbLab3Context : DbContext
+    {
+        public WebbLab3Context (DbContextOptions<WebbLab3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebbLab3.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ WebbLab3/Migrations/20200330170331_Initialcreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebbLab3.Migrations
+{
+    public partial class Initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    NumberOfSeats = table.Column<int>(nullable: false),
+                    MovieStartTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
