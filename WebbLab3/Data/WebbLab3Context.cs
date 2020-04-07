3319e068eca35bf4fa1c4e0962b873a70c336c05
Sunse7 <sunsetluna@gmail.com>
2020-04-07 12:38:15 +0200

    Seed database works

--- WebbLab3/Data/WebbLab3Context.cs
+++ WebbLab3/Data/WebbLab3Context.cs
@@ -15,5 +15,39 @@ namespace WebbLab3.Data
         }
 
         public DbSet<WebbLab3.Models.Movie> Movie { get; set; }
+
+        public void SeedDatabase()
+        {
+            var date = DateTime.Today;
+            var time = DateTime.Now;
+
+            Movie.AddRange(
+                new Movie
+                {
+                    Title = "The Awesome Movie",
+                    NumberOfSeats = 50,
+                    MovieStartTime = date.Date + time.TimeOfDay
+                },
+                new Movie
+                {
+                    Title = "Lucky the Movie",
+                    NumberOfSeats = 50,
+                    MovieStartTime = date.Date + time.TimeOfDay
+                },
+                new Movie
+                {
+                    Title = "Azmodan Gone Wild",
+                    NumberOfSeats = 50,
+                    MovieStartTime = date.Date + time.TimeOfDay
+                },
+                new Movie
+                {
+                    Title = "Big Boii Adventures",
+                    NumberOfSeats = 50,
+                    MovieStartTime = date.Date + time.TimeOfDay
+                });
+
+            SaveChanges();
+        }
     }
 }
---
+++ WebbLab3/Pages/Movies/SeedData.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebbLab3.Data;
+using WebbLab3.Models;
+
+namespace WebbLab3.Pages.Movies
+{
+    public class SeedDataModel : PageModel
+    {
+        private readonly WebbLab3.Data.WebbLab3Context _context;
+
+        public SeedDataModel(WebbLab3.Data.WebbLab3Context context)
+        {
+            _context = context;
+        }
+
+        public bool SeedDatabaseSuccess { get; set; }
+        public bool SeedDatabaseFail { get; set; }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (!_context.Movie.Any())
+            {
+                _context.SeedDatabase();
+                SeedDatabaseSuccess = true;
+            }
+            else
+            {
+                SeedDatabaseFail = true;
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
